7f90c920352687bfa332aba9d5aec6aefa723689
Raj Nanasaheb Patil <146241427+Raj140503@users.noreply.github.com>
2024-03-08 00:07:52 +0530

    Update sendSMS.js

--- sendSMS.js
+++ sendSMS.js
@@ -1,5 +1,5 @@
-const accountSid = 'ACe9386551619f8edc6c0d6077727bf303';
-const authToken = '7aee99c35cffe8b4c94ac23372049106';
+const accountSid = '...............'; Add your own accountSid from Twilio
+const authToken = '................'; Add your own authToken from Twilio
 const client = require('twilio')(accountSid, authToken);
 
 client.messages
